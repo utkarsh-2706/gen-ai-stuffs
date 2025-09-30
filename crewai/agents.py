61a986012f118ac61838d155fdbc49cfde05a4d9
Utkarsh Sharma <sharmautkarsh2706@gmail.com>
2025-09-30 16:38:10 +0530

    Add crew agent files

---
+++ crewai/agents.py
@@ -0,0 +1,47 @@
+from crewai import Agent, LLM
+from tools import tool
+from dotenv import load_dotenv
+load_dotenv('../.env')
+import os
+
+## call the gemini models
+llm = LLM(
+    model="gemini/gemini-1.5-flash-latest",
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+# Creating a senior researcher agent with memory and verbose mode
+
+news_researcher=Agent(
+    role="Senior Researcher",
+    goal='Unccover ground breaking technologies in {topic}',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Driven by curiosity, you're at the forefront of"
+        "innovation, eager to explore and share knowledge that could change"
+        "the world."
+
+    ),
+    tools=[tool],
+    llm=llm,
+    allow_delegation=True
+
+)
+
+## creating a write agent with custom tools responsible in writing news blog
+
+news_writer = Agent(
+  role='Writer',
+  goal='Narrate compelling tech stories about {topic}',
+  verbose=True,
+  memory=True,
+  backstory=(
+    "With a flair for simplifying complex topics, you craft"
+    "engaging narratives that captivate and educate, bringing new"
+    "discoveries to light in an accessible manner."
+  ),
+  tools=[tool],
+  llm=llm,
+  allow_delegation=False
+)
